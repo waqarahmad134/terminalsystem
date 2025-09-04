8a7839e920e25b23b6ccf89a43b7d26ecacb90eb
waqar ahmad <waqar4704649@gmail.com>
2025-09-04 20:48:32 +0500

    next config revert

--- next.config.mjs
+++ next.config.mjs
@@ -11,14 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",                  // frontend request
-        destination: "http://167.71.255.240:8000/:path*", // backend (HTTP)
-      },
-    ]
-  },
 }
 
 export default nextConfig
